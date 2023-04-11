65f28bb050f610ac9353dc8eec3af5c37aed75e8
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-04-11 15:52:23 -0400

    fixed a bug where it was creating a button with an empty string

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,29 +12,50 @@ const humidity = document.querySelectorAll(".humidity");
 const wind = document.querySelectorAll(".wind");
 const dates = document.querySelectorAll(".date");
 const card = document.querySelectorAll(".card-img-top");
+const buttonContainer = document.querySelector(".button-container");
 const savedCities = JSON.parse(localStorage.getItem("cities")) || [];
-
 
 const apiKey = "38a46680df4a9fa4e4fa2e048d140e12";
 
-
 function getWeather() {
   let userInput = input.value.trim();
-  const newButton = document.createElement("button");
-    newButton.textContent = userInput;
-    newButton.classList.add("btn","my-1", "btn-danger", "w-100");
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}&units=imperial`
   )
-    .then((response) => response.json())
-    .then((data) => {
+    .then(function (response) {
+
+      return response.json();
+    })
+    .then(function (data) {
       console.log(data);
-      nowDate.textContent = dayjs(data.dt_txt).format("dddd, MMMM D YYYY");
+      console.log(data.cod);
+      //Checking to see if is a valid city
+      if (data.cod == "404" || data.cod == "400") {
+        alert("Please enter a valid city name");
+        return;
+      }
+      // checking to see if it is already in the array of savedCitties
+      if (!savedCities.includes(userInput)) {
+        console.log("SAVING!!!!!!!!!!!!!");
+        savedCities.push(userInput);
+        localStorage.setItem("cities", JSON.stringify(savedCities));
+        //creates a button and appends to the list of recent searches
+        const newButton = document.createElement("button");
+        newButton.textContent = userInput
+        newButton.classList.add("btn", "my-1", "btn-secondary", "w-100");
+        buttonContainer.appendChild(newButton);
+        newButton.addEventListener("click", () => {
+          input.value = this.innerHTML;
+          getWeather();
+        });
+      }
+
+      nowDate.textContent = dayjs(data.dt_txt).format("MMMM D, YYYY");
       city.textContent = data.name;
-      nowTemp.textContent = `${data.main.temp} F`;
-      nowHum.textContent = `${data.main.humidity}%`;
-      nowWind.textContent = `${data.wind.speed} MPH`;
+      nowTemp.textContent = `Temp: ${data.main.temp} F`;
+      nowHum.textContent = `Humidity: ${data.main.humidity}%`;
+      nowWind.textContent = `Wind: ${data.wind.speed} MPH`;
       nowIMG.setAttribute(
         "src",
         `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
@@ -51,20 +72,20 @@ function getWeather() {
       for (let i = 0; i < data.list.length; i++) {
         let date = new Date(data.list[i].dt_txt);
         console.log(date);
-        temp[0].textContent = `${data.list[4].main.temp} F`;
-        humidity[0].textContent = `${data.list[4].main.humidity}%`;
-        wind[0].textContent = `${data.list[4].wind.speed} MPH`;
+        temp[0].textContent = `Temp: ${data.list[4].main.temp} F`;
+        humidity[0].textContent = `Humidity: ${data.list[4].main.humidity}%`;
+        wind[0].textContent = `Wind: ${data.list[4].wind.speed} MPH`;
         card[0].setAttribute(
           "src",
           `http://openweathermap.org/img/w/${data.list[4].weather[0].icon}.png`
         );
         dates[0].textContent = dayjs(data.list[4].dt_txt).format(
-          "dddd, MMMM D YYYY"
-        );
+          "MMMM D, YYYY")
+      
 
-        temp[1].textContent = `${data.list[12].main.temp} F`;
-        humidity[1].textContent = `${data.list[12].main.humidity}%`;
-        wind[1].textContent = `${data.list[12].wind.speed} MPH`;
+        temp[1].textContent = `Temp: ${data.list[12].main.temp} F`;
+        humidity[1].textContent = `Humidity: ${data.list[12].main.humidity}%`;
+        wind[1].textContent = `Wind ${data.list[12].wind.speed} MPH`;
         card[1].setAttribute(
           "src",
           `http://openweathermap.org/img/w/${data.list[12].weather[0].icon}.png`
@@ -72,15 +93,15 @@ function getWeather() {
         dates[1].textContent = dayjs(data.list[12].dt_txt).format(
           "dddd, MMMM D YYYY"
         );
-        temp[2].textContent = `${data.list[20].main.temp} F`;
-        humidity[2].textContent = `${data.list[20].main.humidity}%`;
-        wind[2].textContent = `${data.list[20].wind.speed} MPH`;
+        temp[2].textContent = `Temp: ${data.list[20].main.temp} F`;
+        humidity[2].textContent = `Humidity: ${data.list[20].main.humidity}%`;
+        wind[2].textContent = `Wind: ${data.list[20].wind.speed} MPH`;
         card[2].setAttribute(
           "src",
           `http://openweathermap.org/img/w/${data.list[20].weather[0].icon}.png`
         );
         dates[2].textContent = dayjs(data.list[20].dt_txt).format(
-          "dddd, MMMM D YYYY"
+          "MMMM D, YYYY"
         );
         temp[3].textContent = `${data.list[28].main.temp} F`;
         humidity[3].textContent = `${data.list[28].main.humidity}%`;
@@ -90,7 +111,7 @@ function getWeather() {
           `http://openweathermap.org/img/w/${data.list[28].weather[0].icon}.png`
         );
         dates[3].textContent = dayjs(data.list[28].dt_txt).format(
-          "dddd, MMMM D YYYY"
+          "MMMM D, YYYY"
         );
         temp[4].textContent = `${data.list[35].main.temp} F`;
         humidity[4].textContent = `${data.list[35].main.humidity}%`;
@@ -100,39 +121,32 @@ function getWeather() {
           `http://openweathermap.org/img/w/${data.list[35].weather[0].icon}.png`
         );
         dates[4].textContent = dayjs(data.list[35].dt_txt).format(
-          "dddd, MMMM D YYYY"
+          "MMMM D, YYYY"
         );
-
       }
-      const buttonContainer = document.querySelector(".button-container");
-buttonContainer.appendChild(newButton);
-
-// save the userInput to local storage
-if (!savedCities.includes(userInput)) {
-  savedCities.push(userInput);
-  localStorage.setItem("cities", JSON.stringify(savedCities));
-}
     });
 }
 
+// save the userInput to local storage
 
-window.addEventListener("load", () => {
-    
-    // Loop through the saved cities and create a button for each one
-    const buttonContainer = document.querySelector(".button-container");
-    for (let i = 0; i < savedCities.length; i++) {
-      const newButton = document.createElement("button");
-      newButton.textContent = savedCities[i];
-      newButton.classList.add("btn","my-1", "btn-danger", "w-100");
-      buttonContainer.appendChild(newButton);
-      newButton.addEventListener("click", () => {
-        input.value = savedCities[i];
-        getWeather();
-      })
-    }
-  });
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    getWeather();
-  })
-  
+function loading() {
+  // Loop through the saved cities and create a button for each one
+  for (let i = 0; i < savedCities.length; i++) {
+    const newButton = document.createElement("button");
+    newButton.textContent = savedCities[i];
+    newButton.classList.add("btn", "my-1", "btn-secondary", "w-100");
+    buttonContainer.appendChild(newButton);
+    newButton.addEventListener("click", () => {
+      input.value = savedCities[i];
+      getWeather();
+    });
+  }
+}
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather();
+});
+
+loading();
